0060f7b3fc5c8d4d697634ba83fd1e4013047dc5
Ejima-we <takumi941503@gmail.com>
2021-02-08 07:16:38 +0000

    profile編集機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd98b846c619597070f24317c421c5d34aa1bd0e42a86552f7358afc10442e7185d1c7b61e3f481b93d535d12f26a636ada266b6aee894dc0476f2d1d6da9e038'
