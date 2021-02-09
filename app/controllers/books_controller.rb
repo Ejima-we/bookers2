49155763bcece53d34a3de51c80106b5f6f2c2d2
Ejima-we <takumi941503@gmail.com>
2021-02-09 08:03:19 +0000

    システム完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
   def show
     @book_new = Book.new
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find(@book.user.id)
   end
 
   def edit
