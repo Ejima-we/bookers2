028e9b30dca059481530de3945c2d18e0bd38569
Ejima-we <takumi941503@gmail.com>
2021-03-01 14:37:43 +0000

    コメント機能作成 削除未

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
-  
+
 end
